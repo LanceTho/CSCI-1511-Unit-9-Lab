2850560691a47c19f1c251ad0a0cfd84d8a56de5
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-10 18:17:46 -0400

    Created the Coin class. Created the __init__ function which initializes the __sideup to a string. Created the toss function which generates a random number of 0 or 1 and sets the coin side to either 'Heads' or 'Tails'. Created the get_sideup function which returns a string that states which side of the coin is facing up.

--- coin.py
+++ coin.py
@@ -4,3 +4,32 @@ Lance Thongsavanh
 This class represents a single, tossable coin. It only knows about its current state (heads or tails).
 Date.
 """
+
+"""
+Attributes:
+    __sideup: A private string attribute, initialized to 'Heads' or 'Tails'.
+
+Methods:
+    __init__(): The initalizer method that sets up the __sideup attribute.
+    toss(): Generates a random timber (0 or 1) and sets __sideup to 'Heads' or 'Tails' accordingly.
+    get_sideup(): Returns the current value of __sideup.
+"""
+import random
+class Coin:
+    def __init__(self, __sideup: str) -> None:
+        self.__sideup = __sideup
+
+    def toss(self) -> None:
+        number: int = random.randint(0,1)
+        if(number == 1):
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"
+
+    def get_sideup(self) -> str:
+        return f"The coin has the {self.__sideup} side facing up."
+    
+if __name__ == "__main__":
+    quarter = Coin("Heads")
+    quarter.toss()
+    print(quarter.get_sideup())
